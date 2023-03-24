b843566f0fde09409bafeba31a048768eb4f6432
DriSidibe <dsidibe653@gmail.com>
2023-03-24 10:09:19 +0000

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-
-from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
-import os
 
-storage = DBStorage() if os.getenv(
-    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
 
+storage = FileStorage()
 storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,16 +8,9 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
+    def all(self):
         """Returns a dictionary of models currently in storage"""
-        tmp_dic = {}
-        if cls is not None:
-            for k, v in FileStorage.__objects.items():
-                if v.__class__.__name__ == cls.__name__:
-                    tmp_dic[k] = v
-        else:
-            return FileStorage.__objects
-        return tmp_dic
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -52,12 +45,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        if obj:
-            if obj in list(FileStorage.__objects.values()):
-                obj_id = obj.to_dict()['__class__'] + "." + obj.id
-                del FileStorage.__objects[obj_id]
